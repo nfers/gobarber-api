8238e0cba49cb1195abbb9d2f14858a3f8cd93b0
Nayara Ferreira <ncy.fers@gmail.com>
2020-07-08 22:02:30 -0300

    implementacao do relacionamento entre usuarios e compromissos

--- src/database/models/Appointment.ts
+++ src/database/models/Appointment.ts
@@ -1,4 +1,12 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import User from './User';
 
 @Entity('appointments')
 class Appointment {
@@ -7,6 +15,10 @@ class Appointment {
 
   @Column()
   provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
   @Column('timestamp with time zone')
   date: Date;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,4 +3,8 @@ import { getCustomRepository } from 'typeorm';
 
 const usersRouter = Router();
 
+usersRouter.get('/', async (req, res) => {
+  return res.status(200).json({ message: 'rota de usuários ok' });
+});
+
 export default usersRouter;
