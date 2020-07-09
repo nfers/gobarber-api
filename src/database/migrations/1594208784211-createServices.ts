deb33b11b41b444e8062b01f1b4ff6e44dc75a2a
Nayara Ferreira <ncy.fers@gmail.com>
2020-07-08 21:53:47 -0300

    criação das migratios de usuarios e alteração de campo da tabela de compromissos

--- src/database/migrations/1594208784211-createServices.ts
+++ src/database/migrations/1594208784211-createServices.ts
@@ -1,28 +1,33 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointmens1594135016086
-  implements MigrationInterface {
+export default class createServices1594208784211 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'services',
         columns: [
           {
             name: 'id',
-            type: 'uuid',
+            type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'service',
             type: 'varchar',
             isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'price',
+            type: 'numeric(5,2)',
+            isNullable: true,
+          },
+          {
+            name: 'isActive',
+            type: 'boolean',
             isNullable: false,
+            default: true,
           },
         ],
       }),
@@ -30,6 +35,6 @@ export default class CreateAppointmens1594135016086
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('services');
   }
 }
---
+++ src/routes/users.routes.ts
@@ -0,0 +1,6 @@
+import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+
+const usersRouter = Router();
+
+export default usersRouter;
